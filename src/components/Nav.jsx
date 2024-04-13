52cbf9b44f400d5423e7fea56f43ae70143735b6
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-13 19:19:48 +0000

    Add @flaticon/flaticon-uicons package to dependencies and update Dashboard page styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,24 +1,118 @@
 import { Feather } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { Search } from 'lucide-react';
+import { useState } from 'react';
+import { Menu } from 'lucide-react';
+import { X } from 'lucide-react';
+import banner3 from '../assets/images/banner3.jpg';
+
+// material ui drawer
+
+
 function Nav() {
+  const [search, setSearch] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const [islogin, setIslogin] = useState(true);
+  const handelLogin = () => {
+    if (islogin) {
+      setIslogin(false);
+    } else {
+      setIslogin(true);
+    }
+  };
+ 
+
   return (
-    <div className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm px-10 w-full ">
-      <div>
+    <div className="navbar  ">
+      <div className='flex items-center justify-center gap-8'>
         <h1 className='flex gap-2'>
-        <Link to="/" className='flex gap-2'>
+        <Link to="/" className='flex gap-2' >
           <Feather  />
           offBlog</Link>
         </h1>
-      </div>
-      <div>
+          {/* search */}
+        <div
+      className={'absolute md:border-0 border-b w-full left-0 top-full mt-0.5 md:show bg-white  border-grey py-3 px-[5vw] md:block md:relative md:inset-0 md:p-0 md:w-auto ' + (search ? 'show' : 'hide')}
+      >
+        <input
+        type='text'
+        placeholder='Search'
+        className='w-full md:w-auto text-sm bg-slate-100 bg-grey p-3  pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-slate-800 focus:outline-none  focus:ring-2 focus:placeholder:text-black focus:ring-black focus:ring-opacity-50 md:pl-12 '
+        />
+        <span
+        className='absolute right-[8%]  md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2'
+        >
+          <Search size={20} />
+        </span>
         
+
       </div>
-      <div className="flex gap-3">
-        <Link to="/testblogs">testblogs</Link>
-      <Link to="/blogs">Blogs</Link>
-        <a href="">About</a>
-        <a href="">FAQ</a> 
+      {/* end search */}
+      
+      </div>
+
+    {/* menu */}
+      
+      <div className={"flex w-full  absolute right-0 top-0  md:relative   justify-start  md:justify-end items-center md:show   "+(menu  ?'show ':'hide') } >
+      
+     
+        <div 
+        className=' absolute  md:relative w-[60%] min-w-[300px]    flex flex-col justify-start py-5 md:py-0  md:justify-end h-screen md:h-auto items-start   md:flex-row bg-white  p-2 mt-0.5 px-5 md:p-0 top-0 md:top-full gap  md:gap-6 gap-3 '
+        >
+          <div className='flex justify-between w-full md:hidden'>
+          <h1 className='flex gap-2 px-3 mb-2 '>
+        <Link to="/" className='flex gap-2' >
+          <Feather  />
+          offBlog</Link>
+          
+         
+        
+        </h1>
+       <button>
+          <X size={30} onClick={() => setMenu(false)} />
+       </button>
+          </div>
+          <Link to="/testblogs" className=' md:text-base navlinks  ' >testblogs</Link>
+          
+       
+      <Link to="/blogs " className=' md:text-base navlinks'>Blogs</Link>
+        <Link  to="/about " className=' md:text-base navlinks'>About</Link>
+        <Link to="/faq " className=' md:text-base navlinks'>FAQ</Link> 
+
+        <Link to="/login" className='text-sm md:text-base btn-dark py-2 md:hidden block w-full text-center '>Login</Link>
+          <Link to="/register" className='text-sm md:text-base btn-light py-2 md:hidden block w-full text-center  '>Register</Link>
         </div>
+       
+        </div>
+        {/* end menu */}
+
+        {/* {login profil} */}
+       
+       
+        <div className='flex items-center gap-3 md:gap-4 ml-auto    '>
+          <button className='md:hidden bg-gray-100 w-10 h-10 rounded-full flex items-center justify-center'
+          onClick={() => {setSearch(!search), setMenu(false)}}
+          >
+            <Search size={20}/>
+          </button>
+          <Link to="/login" className='text-sm md:text-base btn-dark py-2 hidden md:block' >Login</Link>
+          <Link to="/register" className='text-sm md:text-base btn-light py-2  hidden md:block'>Register</Link>
+          
+          
+          
+        </div>
+        <div 
+        className=' md:hidden  items-center flex    '
+        >
+          <button
+         onClick={() => {setMenu(!menu) ,setSearch(false)}}
+          >
+          <Menu size={30} />
+          </button>
+        </div>
+        
+       
+       
     </div>
   );
 }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div 
-    className=' grid grid-cols-12   gap-5 justify-start my-10 mx-20'
+    className=' grid grid-cols-12   gap-5 justify-start my-10 mx-20 bg-white rounded-lg p-5'
     >
         <div
         className='col-span-2'
