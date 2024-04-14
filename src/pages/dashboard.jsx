d3b24f8487bcac72bcfc4dc0cf3c95483228824b
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-14 18:57:14 +0100

    Update grid layout and margin in dashboard.jsx

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -13,7 +13,7 @@ function Dashboard() {
   return (
     <>
     <div 
-    className=' grid grid-cols-12   gap-5 justify-start my-10 mx-20 bg-white rounded-lg p-5'
+    className=' grid grid-cols-12   gap-5 justify-start my-10 mx-5 md:mx-20 bg-white rounded-lg p-5'
     >
         <div
         className='col-span-2  border-r border-gray-200 pr-5'
