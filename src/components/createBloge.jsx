b25c593a13f3e3da62956c6cad2ee293cf7671b4
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-17 19:22:19 +0100

    Refactor createBloge.jsx to update image state variable and add submit functionality

--- src/components/createBloge.jsx
+++ src/components/createBloge.jsx
@@ -6,7 +6,10 @@ function CreateBloge() {
   const [content, setContent] = useState("");
   const [user_id, setUser_id] = useState("");
   const [category_id, setCategory_id] = useState("");
-  const [image, setImage] = useState("");
+    const [image_url, setImage] = useState("");
+  const handelsubmit = () => {
+    console.log(title, content, user_id, category_id, image_url);
+  }
   return (
     <div className="flex flex-col gap-2 justify-center items-center h-screen">
       <div className=" max-w-[515px]  gap-2 flex flex-col w-full  ">
@@ -45,11 +48,16 @@ function CreateBloge() {
         />
         <input
           type="file"
-          value={image}
+          value={image_url}
           onChange={(e) => setImage(e.target.value)}
           placeholder="image"
           className="md:w-auto text-sm w-full bg-slate-100 bg-grey p-3  pl-6 pr-[12%] md:pr-6 rounded-lg placeholder:text-slate-500 focus:outline-none  focus:ring-2 focus:placeholder:text-black focus:ring-black focus:ring-opacity-50 md:pl-12 "
         />
+        <button
+        onClick={handelsubmit}
+        className="btn-light py-2 text-sm mt-2 flex justify-center w-full">
+          create
+        </button>
       </div>
     </div>
   );
