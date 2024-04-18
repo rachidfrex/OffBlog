8ca54984d930eadd6d1f6dd9a5accfd129ad006e
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-18 17:16:06 +0100

    Refactor BlogEditor component in blogEditor.jsx to handle image upload and title input

--- src/components/blogEditor.jsx
+++ src/components/blogEditor.jsx
@@ -1,9 +1,27 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Feather } from "lucide-react";
-import banner from "../assets/images/blog banner.png";
+import bannerimg from "../assets/images/blog banner.png";
+import { useState } from "react";
 
 function BlogEditor() {
+
+  const [banner, setBanner] = useState(bannerimg); 
+
+  const handelBnnerUpload = (e) => {
+    let img = e.target.files[0]; 
+    console.log(img);
+    setBanner(URL.createObjectURL(img));
+  };
+  const handelTitleKeyDonw = (e) => {
+    if(e.keyCode === 13){
+      e.preventDefault();
+    }
+  };
+  const handelTitleChange = (e) => {
+    let title = e.target;
+  title.style.height = "auto";
+  title.style.height = title.scrollHeight + "px";
+  };
   return (
     <>
       <nav className="navbar">
@@ -22,9 +40,17 @@ function BlogEditor() {
         <div className="mx-auto max-w-[900px] w-full">
           <div className="relative aspect-video bg-white border-4 border-slate-300 hover:opacity-80">
             <label htmlFor="uploadBanner">
-              <img src={banner} alt="" />
-              <input type="file" name="image" id="uploadBanner" accept=".png , .jpg ,jpeg " hidden />
+              <img src={banner} alt="" className="z-20"  />
+              <input type="file" name="image" id="uploadBanner" accept=".png , .jpg ,jpeg " hidden onChange={handelBnnerUpload} />
             </label>
+          </div>
+          <div>
+            <textarea
+            placeholder="blog title "
+            className="text-4xl font-medium w-full  h-20 border-none  outline-none resize-none mt-10 leading-tight placeholder:opacity-40 overflow-hidden focus:ring-0 "
+             onKeyDown={handelTitleKeyDonw}
+             onChange={handelTitleChange}
+             name="" id=""  ></textarea>
           </div>
         </div>
       </section>
