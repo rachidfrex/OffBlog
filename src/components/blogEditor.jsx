957bc1d10aa6ad5e8e25637d2eb696c6ae52d974
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-18 16:47:11 +0100

    Update BlogEditor component in blogEditor.jsx

--- src/components/blogEditor.jsx
+++ src/components/blogEditor.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Feather } from "lucide-react";
+import banner from "../assets/images/blog banner.png";
 
 function BlogEditor() {
   return (
-    <nav className='navbar'>
-      <Link to='/'>
-        <img src=" " alt="" />
-      </Link>
-    </nav>
-  )
+    <>
+      <nav className="navbar">
+        <Link to="/" className="flex-none w-5  ">
+          <Feather />
+        </Link>
+        <p className="max-md:hiddnen text-black line-clamp-1 w-full">
+          new blog
+        </p>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-dark py-2 text-sm">publish</button>
+          <button className="btn-light py-2 text-sm">save draft</button>
+        </div>
+      </nav>
+      <section>
+        <div className="mx-auto max-w-[900px] w-full">
+          <div className="relative aspect-video bg-white border-4 border-slate-300 hover:opacity-80">
+            <label htmlFor="uploadBanner">
+              <img src={banner} alt="" />
+              <input type="file" name="image" id="uploadBanner" accept=".png , .jpg ,jpeg " hidden />
+            </label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default BlogEditor
+export default BlogEditor;
