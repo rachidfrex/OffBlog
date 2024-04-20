20cc17313ce761920e53694a1862555107574584
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-20 15:42:00 +0100

    Refactor tool.component.jsx to include new editorjs plugins

--- src/components/tool.component.jsx
+++ src/components/tool.component.jsx
@@ -5,3 +5,13 @@ import Header from '@editorjs/header';
 import Quote from '@editorjs/quote';
 import Marker from '@editorjs/marker';
 import InlineCode from '@editorjs/inline-code';
+export const tools = {
+    embed: Embed,
+    list: List,
+    image: Image,
+    header: Header,
+    quote: Quote,
+    marker: Marker,
+    inlineCode: InlineCode
+    };
+    
