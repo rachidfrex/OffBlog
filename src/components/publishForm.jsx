726073e87fedc2d523f080a3369036b3e1b90a49
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-20 21:51:33 +0100

    Refactor PublishForm component in publishForm.jsx to include character limit for blog description and handle category input

--- src/components/publishForm.jsx
+++ src/components/publishForm.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react'
 import { Toaster ,toast } from 'react-hot-toast'
 import { EdidoreContext } from "./editorPage";
+import { Tags } from 'lucide-react';
 function PublishForm() {
+    const characterLimit = 200;
     let {blog ,blog:{title ,image_url,content,category,user_id ,des} ,setEditorState ,setBlog} = useContext(EdidoreContext);
     const handekCloseEvent = () => {
         setEditorState("editor");
@@ -11,8 +13,17 @@ function PublishForm() {
 
     const handelBlogTilteChange = (e) => {
         let title = e.target.value;
-        setBlog({...blog, title: e.target.value});
+        setBlog({...blog, title: title});
     }
+    const handelBlogDesChange = (e) => {
+        let des = e.target.value;
+        setBlog({...blog, des: des});
+    }
+    const handelTitleKeyDonw = (e) => {
+        if(e.keyCode === 13){
+          e.preventDefault();
+        }
+         }
   return (
     <>
     <section className='w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4'>
@@ -25,7 +36,7 @@ function PublishForm() {
         <div className='max-w-[550px] center' >
             <p className=' mb-1 '>preview</p>
             <div className='w-full aspect-video rounded-lg overflow-hidden bg-slate-300'>
-                <img src={image_url} alt="" />
+                <img src={image_url} alt="" className='spect-video' />
             </div>
             <h1 className='text-4xl font-medium mt-2 leading-tight line-clamp-2'>
                 {title}
@@ -37,9 +48,23 @@ function PublishForm() {
         </div>
         <div className='border-grey lg:border lg:pl-8'>
             <p className='mb-2 mt-9'>Blog Title</p>
-            <input type="text" value={title} placeholder='Blog Tite' className='input-box pl-4 '  onChange={handelBlogTilteChange} />
+            <input type="text" value={title} placeholder='Blog Tite' className='input-box pl-4 bg-slate-200  '  onChange={handelBlogTilteChange} />
             <p className='mb-2 mt-9'>short desctiption about your blog  </p>
-            <input type="text" value={des} placeholder='Blog Tite' className='input-box pl-4 '  />
+            <textarea 
+            onKeyDown={handelTitleKeyDonw}
+            maxLength={characterLimit}
+            defaultValue={des}
+            placeholder='short desctiption about your blog'
+            onChange={handelBlogDesChange}
+            className='h-40 resize-none leading-7 input-box pl-4 bg-slate-200 '
+            name="" id="" cols="30" rows="10"></textarea>
+            <p className='mt-1 text-sm text-right'>{characterLimit - des.length } characters left</p>
+            <p className='mb-2 mt-9'> category  </p>
+            <div className='relative input-box bg-slate-200 pl-2 py-2 pb-4 '>
+                <input type="text" placeholder='category' 
+                className='sticky input-box bg-white top-0 left-0 pl-4 mb-3 focus:bg-white'/>
+                <Tags />
+            </div>
         </div>
     </section>
     </>
