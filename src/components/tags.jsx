4d44b949fc535d2fbe1ee615f36675df123213a0
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-20 23:03:16 +0100

    Refactor Tags component in tags.jsx to handle tag editing and deletion

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -3,7 +3,12 @@ import { useContext } from 'react';
 import { EdidoreContext } from './editorPage';
 const Tags = ({tags}) => {
     const {blog ,setBlog ,blog:{category}} = useContext(EdidoreContext);
-
+    const handeltageEdit = (e) => {
+        if(e.keyCode === 13 || e.keyCode === 188){
+            e.preventDefault();
+            
+        }
+    }
     const handelTagDelete = () => {
         tags = category.filter((tag) => tag !== tags);
         setBlog({...blog, category: tags});
@@ -11,7 +16,7 @@ const Tags = ({tags}) => {
     }
     return (
         <div className='relative bg-white p-1 mt-2 mr-2 px-5 rounded-full inline-block pr-6  transform  duration-200 ease-in-out '>
-           <p className="outline-none text-sm" contentEditable="true">{tags}</p>
+           <p className="outline-none text-sm" onKeyDown={handeltageEdit} contentEditable="true">{tags}</p>
             <button className='  rounded-full absolute right-1 top-1/4 -trasnlate-y-1/2 '
             onClick={handelTagDelete}
             >
