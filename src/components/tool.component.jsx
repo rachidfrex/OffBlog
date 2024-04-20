4c6c350ec045ac0f348b30ed218ce424411019b4
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-20 15:38:11 +0100

    Refactor package dependencies to include new editorjs plugins

---
+++ src/components/tool.component.jsx
@@ -0,0 +1,7 @@
+import Embed from '@editorjs/embed';
+import List from '@editorjs/list';
+import Image from '@editorjs/image';
+import Header from '@editorjs/header';
+import Quote from '@editorjs/quote';
+import Marker from '@editorjs/marker';
+import InlineCode from '@editorjs/inline-code';
