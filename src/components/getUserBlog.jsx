0ca6b0138e9996ae265f4aefce6eb1664634efbe
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-25 21:07:50 +0100

    Refactor GetUserBlog component to use an array for userblogs state in getUserBlog.jsx

--- src/components/getUserBlog.jsx
+++ src/components/getUserBlog.jsx
@@ -1,9 +1,7 @@
 import P from "@editorjs/image";
 import { useState ,useEffect } from "react"
 function GetUserBlog() {
-    const [userblogs, setUserBlogs] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
-    // get the user id using local storage
+    const [userblogs, setUserBlogs] = useState([]);
     const userInfo = JSON.parse(localStorage.getItem('user-info'));
     const user_id = userInfo ? userInfo.user_id : null;
     const handelGetuserBlogs = async () => {
@@ -16,15 +14,16 @@ function GetUserBlog() {
         });
         blogs = await blogs.json();
         console.log("the blogs", blogs); 
-        setUserBlogs(blogs);
-        console.log("test blogs", userblogs);
+         setUserBlogs(blogs);
+        
 
-        setIsLoading(true);
 
       };
+
     
       useEffect(() => {
         handelGetuserBlogs();
+
       }, []);
 
   return (
@@ -34,11 +33,30 @@ function GetUserBlog() {
         </h1>
         <div>
         {
-            // allblogs.map((blogs) => ( 
-            //     <p>
-            //         {blogs.title}
-            //     </p>
-            // ) )
+          userblogs.map((blog) => (
+            <div key={blog.id} className="border items-center border-gray-200 p-2 mt-2 rounded-md grid grid-cols-5 w-full">
+              <div className=" col-span-1">
+                <img 
+                className="object-center w-40 h-40"
+                src={`http://localhost:8000${blog.image_url}`}
+                alt="" />
+              </div>
+              <div className="w-full col-span-4 ">
+                <h1 className="font-semibold">{blog.title}</h1>
+                <p className="line-clamp-3 text-sm text-gray-500">
+                  {
+                    JSON.parse(blog.content).map((item, index) => (
+                      // i want to displat the first 3 paragraph of the blog
+                      item.type === 'paragraph' && <p
+                       className=''
+                      key={index}>{item.data.text}</p>
+
+                    ))
+                  }
+                </p>
+              </div>
+            </div>
+          ))
         
         }
         </div>
