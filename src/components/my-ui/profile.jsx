c1017b99a44617db950991bf0fb20aab1dce0707
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-25 20:08:40 +0100

    Update profile dropdown styles, add dropdownProfil class, and update links in profile.jsx

--- src/components/my-ui/profile.jsx
+++ src/components/my-ui/profile.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Profile() {
   return (
-    <div className="cursor-pointer flex flex-col dropdownProfil">
-      <ul className="flex flex-col gap-6">
-        <li>profile</li>
-        <li>Dachboard</li>
-        <li>Notifications</li>
-        <li>log out</li>
+    <div className="cursor-pointer flex flex-col  text-sm">
+      <ul className="flex flex-col  dropdownProfil">
+        <li className="hover:bg-gray-50 px-4 py-1 ">
+          <Link to="/dashboard/edit-profile">Profile</Link>
+        </li>
+        <li className="hover:bg-gray-50 px-4  py-1   ">
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li className="hover:bg-gray-50 px-4  py-1  ">
+          <Link to="/dashboard/change-password">Notifications</Link>
+        </li>
+        <hr className="  mx-2 border-black/30 mt-2"/>
+        <li className="hover:bg-gray-50 px-4  pt-2  ">log out</li>
       </ul>
     </div>
   );
