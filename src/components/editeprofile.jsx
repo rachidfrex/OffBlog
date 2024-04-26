c43c776c4be26a7bebaa053a44af339b993b40fa
rachidfrex <2003043000250@ofppt-edu.ma>
2024-04-26 19:34:08 +0100

    Update profile image handling and display in editeprofile.jsx

--- src/components/editeprofile.jsx
+++ src/components/editeprofile.jsx
@@ -2,25 +2,28 @@ import React, { useEffect } from "react";
 import { User, UserRound } from "lucide-react";
 import { Mail } from "lucide-react";
 import { useState } from "react";
-import prodil from "../assets/images/profil.jpg";
+import profile from "../assets/images/profil.jpg";
 import { Link, Outlet } from "react-router-dom";
-import { AtSign } from 'lucide-react';
-
+import { AtSign } from "lucide-react";
 
 function Editeprofile() {
   const [textCounter, setTextCounter] = useState(200);
-  const [user, setUser] = useState( {});
-
+  const [user, setUser] = useState({});
+  const [profileImage, setProfileImage] = useState(
+    `http://localhost:8000${user.profile_image}` || profile
+  );
   const handleTextChange = (event) => {
     setTextCounter(200 - event.target.value.length);
   };
 
-  // i want to get the user data from the storage 
- 
-  
- 
+  const handleImageUpload = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setProfileImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
   useEffect(() => {
-    const userInfo = JSON.parse(localStorage.getItem('user-info'));
+    const userInfo = JSON.parse(localStorage.getItem("user-info"));
     const user_id = userInfo ? userInfo.user_id : null;
     const getUser = async () => {
       let result = await fetch(`http://localhost:8000/api/user/${user_id}`, {
@@ -34,8 +37,7 @@ function Editeprofile() {
     getUser();
   }, []);
 
-  console.log("the user info ",user);
-
+  console.log("the user info ", user);
 
   return (
     <>
@@ -46,13 +48,16 @@ function Editeprofile() {
         <div className=" grid grid-cols-12   gap-5 mt-5">
           <div className=" col-span-2 flex flex-col  items-center">
             <img
-              src={`http://localhost:8000${user.profile_image}` || prodil}
+              src={profileImage}
               alt=""
               className="w-32 h-32 border-2 rounded-full  object-center "
             />
-            <button className="btn-light py-2 text-sm mt-2 flex justify-center w-full">
-              upload
-            </button>
+            <input
+              type="file"
+              onChange={handleImageUpload}
+              hidden
+              className="btn-light py-2 text-sm mt-2 flex justify-center w-full"
+            />
           </div>
           <div className=" col-span-8">
             <form action="">
@@ -75,7 +80,7 @@ function Editeprofile() {
                     placeholder="email"
                     className="w-full md:w-auto text-sm bg-slate-100 bg-grey p-3  pl-6 pr-[12%] md:pr-6 rounded-lg placeholder:text-slate-500 focus:outline-none  focus:ring-2 focus:placeholder:text-black focus:ring-black focus:ring-opacity-50 md:pl-12 "
                   />
-                  
+
                   <span className="absolute right-[8%]  md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2">
                     <Mail size={15} />
                   </span>
@@ -84,7 +89,6 @@ function Editeprofile() {
               <div className=" mt-5 className={'absolute md:border-0 border-b w-full left-0 top-full mt-0.5 md:show bg-white  border-grey py-3 px-[5vw] md:block md:relative md:inset-0 md:p-0 md:w-auto ' + (search ? 'show' : 'hide')}">
                 <input
                   type="text"
-
                   placeholder="user name"
                   className="w-full md:w-[515px] text-sm pr-[12%]  bg-slate-100 bg-grey p-3  pl-6 md:pr-6 rounded-lg placeholder:text-slate-500 focus:outline-none  focus:ring-2 focus:placeholder:text-black focus:ring-black focus:ring-opacity-50 md:pl-12 "
                 />
@@ -107,7 +111,6 @@ function Editeprofile() {
                   {textCounter} characters remaining
                 </span>
               </div>
-              
             </form>
           </div>
         </div>
