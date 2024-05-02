1e97e01e1ac5735f11dc644982e0da56b2709a84
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-02 15:12:50 +0100

    Refactor footer component to update styles and add offBlog link in footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,66 +9,96 @@ function Footer() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div>
-      <div className="bg-black/90 text-white p-10 mt-20 mx-20 m-5 rounded-lg ">
-        <div className="flex justify-between">
-          <div>
-            <h1 className="text-xl">OffStore</h1>
-            <div className="flex gap-2 mt-4">
-              <p>
-                <Instagram />
-              </p>
-              <p>
-                <Facebook />
-              </p>
-              <p>
-                <Twitter />
-              </p>
-              <p>
-                <Youtube />
-              </p>
-            </div>
-          </div>
-          <div>
-            <h1 className="text-xl">INFO </h1>
-            <div className="flex flex-col gap-3 mt-4">
-              <p>products</p>
-              <p>collections</p>
-              <p>about</p>
-              <p>FAQ</p>
-            </div>
-          </div>
-          <div>
-            <h1 className="text-xl">RECOURCES </h1>
-            <div className="flex flex-col gap-3 mt-4">
-              <p>contact</p>
-              <p>support</p>
-              <p>privacy</p>
-              <p>terms</p>
-              <p>blog</p>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center  items-start ">
-            <h1 className="text-xl">Subscribe to our newsletter</h1>
-            <div className="flex gap-2">
-              <input
-                type="text"
-                className="  p-2 rounded-md w-full mt-4 text-black "
-              />
-              <button className="bg-white text-black p-2 rounded-md mt-4 ">
-                Subscribe
-              </button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <p className="text-center mt-10">
-            &copy; 2021 OffStore. All rights reserved.
-          </p>
+    // <div>
+    //   <div className="bg-black/90 text-white p-10 mt-20 mx-20 m-5 rounded-lg ">
+    //     <div className="flex justify-between">
+    //       <div>
+    //         <h1 className="text-xl">OffStore</h1>
+    //         <div className="flex gap-2 mt-4">
+    //           <p>
+    //             <Instagram />
+    //           </p>
+    //           <p>
+    //             <Facebook />
+    //           </p>
+    //           <p>
+    //             <Twitter />
+    //           </p>
+    //           <p>
+    //             <Youtube />
+    //           </p>
+    //         </div>
+    //       </div>
+    //       <div>
+    //         <h1 className="text-xl">INFO </h1>
+    //         <div className="flex flex-col gap-3 mt-4">
+    //           <p>products</p>
+    //           <p>collections</p>
+    //           <p>about</p>
+    //           <p>FAQ</p>
+    //         </div>
+    //       </div>
+    //       <div>
+    //         <h1 className="text-xl">RECOURCES </h1>
+    //         <div className="flex flex-col gap-3 mt-4">
+    //           <p>contact</p>
+    //           <p>support</p>
+    //           <p>privacy</p>
+    //           <p>terms</p>
+    //           <p>blog</p>
+    //         </div>
+    //       </div>
+    //       <div className="flex flex-col justify-center  items-start ">
+    //         <h1 className="text-xl">Subscribe to our newsletter</h1>
+    //         <div className="flex gap-2">
+    //           <input
+    //             type="text"
+    //             className="  p-2 rounded-md w-full mt-4 text-black "
+    //           />
+    //           <button className="bg-white text-black p-2 rounded-md mt-4 ">
+    //             Subscribe
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div>
+    //       <p className="text-center mt-10">
+    //         &copy; 2021 OffStore. All rights reserved.
+    //       </p>
          
+    //     </div>
+    //   </div>
+    // </div>
+    
+
+<footer className="bg-black/90 rounded-lg shadow  m-4">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">offBlog</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">FAQ</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Blogs</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
         </div>
-      </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">offBlog™</a>. All Rights Reserved.</span>
     </div>
+</footer>
+
+
   );
 }
 
--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -38,7 +38,7 @@ function Blog() {
   
   return (
     //mt-10 mx-5 max-w-[1536px]   lg:mx-20  pt-5 flex flex-col gap-5 justify-center items-center bg-white  rounded-lg 
-    <div className="mt-10 flex flex-col gap-5 justify-center items-center  ">
+    <div className="mt-10 flex flex-col gap-5 justify-center items-center lg:mx-10  ">
       <div className="flex  justify-between items-center w-full px-5 transition duration-300 ease-out cursor-pointer">
         <div className="flex items-center justify-center ">
           <ChevronLeft />
@@ -46,15 +46,15 @@ function Blog() {
         </div>
         <div className="flex  gap-5 text-sm md:text-balance ">
          <div className="flex gap-1">
-          <Heart className="hover:text-red-500 cursor-pointer " /> 
+          <Heart size={20} className="hover:text-red-500 cursor-pointer " /> 
           <p>12</p>
          </div >
          <div className="flex gap-1">
-          <Bookmark className="hover:text-yellow-500 cursor-pointer" />
+          <Bookmark size={20} className="hover:text-yellow-500 cursor-pointer" />
           <p>20</p>
           </div>
           <div className="flex gap-1">
-          <Eye className="hover:text-blue-500 cursor-pointer" />
+          <Eye size={20} className="hover:text-blue-500 cursor-pointer" />
           <p>30</p>
           </div>
         </div>
@@ -63,11 +63,13 @@ function Blog() {
         {/* this is the blog start */}
         {isLoading ? (
         <>
+         
           <BlogSkeleton />
+        
           <p>loakn fw f </p>
         </>
 ) : (
-  <div className=" w-full">
+  <div className=" w-full ">
      <div className="flex gap-3 justify-center items-center  ">
      {theblog.categories && theblog.categories.slice(0, 3).map((category, index) => (
       <span key={index}  className="px-5 py-[5px] bg-slate-100 shadow-sm rounded-full">
@@ -77,7 +79,7 @@ function Blog() {
       </div>
     {/* this is the blogs title */}
     <div className="flex justify-center py-5">
-      <h1 className=" text-3xl md:text-5xl px-10 md:px-0 flex  justify-center items-center text-center w-[700px]  font-semibold ">
+      <h1 className=" text-2xl md:text-5xl px-10 md:px-0 flex  justify-center items-center text-center w-full md:w-[700px]  font-semibold ">
         {theblog.title}
       </h1>
     </div>
@@ -91,18 +93,18 @@ function Blog() {
         </p>
       </div>
     {/* this is the blogs image */}
-    <div className="px-5 w- full flex justify-center">
+    <div className="px-5 w-full flex justify-center">
       <img
         src={`http://localhost:8000${theblog.image_url}`}
-        className=" object-center  h-[480px] max-w-[1000px]    w-full rounded-xl  aspect-video "
+        className=" object-center w-full  md:h-[480px] max-w-[1000px]     rounded-xl  aspect-video "
       />
     </div>
     {/* this is the blogs content */}
-    <div className=" flex flex-col md:grid md:grid-cols-12  justify-start w-full  gap-5 px-10 py-10">
-      <div className=" col-span-3">
+    <div className=" flex flex-col lg:grid lg:grid-cols-12  justify-start w-full  gap-5 px-5 md:px-10 py-10">
+      <div className=" col-span-3  lg:col-span-4">
         <TableofCentent content={theblog.content} />
       </div>
-      <div className="col-span-8 text-justify">
+      <div className=" col-span-9 lg:col-span-8 text-justify">
         {
           JSON.parse(theblog.content).map((item, index) => {
             switch (item.type) {
@@ -132,85 +134,79 @@ function Blog() {
 )}
        {/* this is the end of the blogs  */}
       {/* this is the blogs Comments */}
-      <div className="flex flex-col justify-start items-start gap-5 px-40 w-full ">
-        <h1 className="text-2xl font-bold">Comments (2)</h1>
-        <div className="flex flex-col gap-5">
-         
-          <textarea name="" id="" cols="100" rows="3"
-          placeholder="write your comment..."
-          className="w-full p-5 rounded-lg border-2 border-slate-200"
-          ></textarea>
-          <button className="bg-black w-[200px] text-white p-2 rounded-lg px-10 border-2 hover:bg-white hover:border-2 hover:border-black hover:text-black transition duration-300 ease-in-out">
-            Post Comment
-          </button>
-          </div>
-          {/* this is the comment */}
-          <div>
-          <div className="flex gap-5 mx-5 py-5">
-            <div className="w-[50px] h-[50px] rounded-full bg-black"></div>
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between items-center  ">
-              <h1 className="text-lg font-bold">John Doe</h1>
-              <span
-              className="text-slate-500  rounded-lg px-2  flex items-start  hover:bg-slate-200  cursor-pointer"
-              >
-                <Ellipsis />
-              </span>
-              </div>
-              <div 
-              className=" text-sm"
-              >
-                <div className="bg-slate-100 px-5 py-3 rounded-lg rounded-bl-2xl w-[500px] text-slate-700 text-sm" >
-                <p
-                className="text-justify"
-                >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                euismod and the man is potato
-                </p>
-              </div>
-             <div className="flex gap-2 px-4 pt-2">
-             <p className="text-slate-500">March 20, 2021</p>
-             {"."}
-             <p className="text-slate-500">reply</p>
-             {"."}
-             <p className="text-slate-500 hover:text-red-500 transform duration-300 ease-in cursor-pointer">like</p>
-                </div>
-                
-                
-             </div>
-             
-             {/* this is the reply comment */}
-             <div className="flex gap-5 mx-5 py-5">
-            <div className="w-[50px] h-[50px] rounded-full bg-black"></div>
-            <div className="flex flex-col gap-2">
-              <div className="flex justify-between items-center  ">
-              <h1 className="text-lg font-bold">Ahmed Hassan</h1>
-              <span
-              className="text-slate-500  rounded-lg px-2  flex items-start  hover:bg-slate-200  cursor-pointer"
-              >
-                <Ellipsis />
-              </span>
-              </div>
-              <div 
-              className="text-sm"
-              >
-                <div>
-                <p
-                className="text-justify  px-5 py-3 bg-slate-100 p-5 rounded-lg rounded-bl-2xl w-[500px] text-slate-700"
-                >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                </p>
+      <section className=" w-full relative flex items-center justify-center antialiased bg-white ">
+    <div className="container px-0 mx-auto sm:px-5">
 
-</div>
-             </div>
-             
-             </div>
-             </div>
-              
+        <div className="flex-col w-full py-4 mx-auto bg-white border-b-2 border-r-2 border-gray-200 sm:px-4 sm:py-4 md:px-4 sm:rounded-lg sm:shadow-sm md:w-2/3">
+            <div className="flex flex-row">
+                <img className="object-cover w-12 h-12 border-2 border-gray-300 rounded-full" alt="Noob master's avatar" src="https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&faces=1&faceindex=1&facepad=2.5&w=500&h=500&q=80" />
+                <div className="flex-col mt-1">
+                    <div className="flex items-center flex-1 px-4 font-bold leading-tight">Noob master
+                        <span className="ml-2 text-xs font-normal text-gray-500">2 weeks ago</span>
+                    </div>
+                    <div className="flex-1 px-2 ml-2 text-sm font-medium leading-loose text-gray-600">Wow!!! how you did you create this?
+                    </div>
+                    <button className="inline-flex items-center px-1 pt-2 ml-1 flex-column">
+                        <svg className="w-5 h-5 ml-2 text-gray-600 cursor-pointer fill-current hover:text-gray-900" viewBox="0 0 95 78" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M29.58 0c1.53.064 2.88 1.47 2.879 3v11.31c19.841.769 34.384 8.902 41.247 20.464 7.212 12.15 5.505 27.83-6.384 40.273-.987 1.088-2.82 1.274-4.005.405-1.186-.868-1.559-2.67-.814-3.936 4.986-9.075 2.985-18.092-3.13-24.214-5.775-5.78-15.377-8.782-26.914-5.53V53.99c-.01 1.167-.769 2.294-1.848 2.744-1.08.45-2.416.195-3.253-.62L.85 30.119c-1.146-1.124-1.131-3.205.032-4.312L27.389.812c.703-.579 1.49-.703 2.19-.812zm-3.13 9.935L7.297 27.994l19.153 18.84v-7.342c-.002-1.244.856-2.442 2.034-2.844 14.307-4.882 27.323-1.394 35.145 6.437 3.985 3.989 6.581 9.143 7.355 14.715 2.14-6.959 1.157-13.902-2.441-19.964-5.89-9.92-19.251-17.684-39.089-17.684-1.573 0-3.004-1.429-3.004-3V9.936z" fill-rule="nonzero" />
+                        </svg>
+                    </button>
+                    <button className="inline-flex items-center px-1 -ml-1 flex-column">
+                        <svg className="w-5 h-5 text-gray-600 cursor-pointer hover:text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
+                        </svg>
+                    </button>
+                </div>
             </div>
+            <hr className="my-2 ml-16 border-gray-200" />
+            <div className="flex flex-row pt-1 md-10 md:ml-16">
+                <img className="w-12 h-12 border-2 border-gray-300 rounded-full" alt="Emily's avatar" src="https://images.unsplash.com/photo-1581624657276-5807462d0a3a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&fit=facearea&faces=1&faceindex=1&facepad=2.5&w=500&h=500&q=80" />
+                <div className="flex-col mt-1">
+                    <div className="flex items-center flex-1 px-4 font-bold leading-tight">Emily
+                        <span className="ml-2 text-xs font-normal text-gray-500">5 days ago</span>
+                    </div>
+                    <div className="flex-1 px-2 ml-2 text-sm font-medium leading-loose text-gray-600">I created it using TailwindCSS
+                    </div>
+                    <button className="inline-flex items-center px-1 pt-2 ml-1 flex-column">
+                        <svg className="w-5 h-5 ml-2 text-gray-600 cursor-pointer fill-current hover:text-gray-900" viewBox="0 0 95 78" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M29.58 0c1.53.064 2.88 1.47 2.879 3v11.31c19.841.769 34.384 8.902 41.247 20.464 7.212 12.15 5.505 27.83-6.384 40.273-.987 1.088-2.82 1.274-4.005.405-1.186-.868-1.559-2.67-.814-3.936 4.986-9.075 2.985-18.092-3.13-24.214-5.775-5.78-15.377-8.782-26.914-5.53V53.99c-.01 1.167-.769 2.294-1.848 2.744-1.08.45-2.416.195-3.253-.62L.85 30.119c-1.146-1.124-1.131-3.205.032-4.312L27.389.812c.703-.579 1.49-.703 2.19-.812zm-3.13 9.935L7.297 27.994l19.153 18.84v-7.342c-.002-1.244.856-2.442 2.034-2.844 14.307-4.882 27.323-1.394 35.145 6.437 3.985 3.989 6.581 9.143 7.355 14.715 2.14-6.959 1.157-13.902-2.441-19.964-5.89-9.92-19.251-17.684-39.089-17.684-1.573 0-3.004-1.429-3.004-3V9.936z" fill-rule="nonzero" />
+                        </svg>
+                    </button>
+                    <button className="inline-flex items-center px-1 -ml-1 flex-column">
+                        <svg className="w-5 h-5 text-gray-600 cursor-pointer hover:text-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
+                        </svg>
+                    </button>
+                </div>
             </div>
-          </div>
+        </div>
+
+        <div className="flex-col w-full py-4 mx-auto mt-3 bg-white border-b-2 border-r-2 border-gray-200 sm:px-4 sm:py-4 md:px-4 sm:rounded-lg sm:shadow-sm md:w-2/3">
+            <div className="flex flex-row md-10">
+                <img className="w-12 h-12 border-2 border-gray-300 rounded-full" alt="Anonymous's avatar" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&faces=1&faceindex=1&facepad=2.5&w=500&h=500&q=80" />
+                <div className="flex-col mt-1">
+                    <div className="flex items-center flex-1 px-4 font-bold leading-tight">Anonymous
+                        <span className="ml-2 text-xs font-normal text-gray-500">3 days ago</span>
+                    </div>
+                    <div className="flex-1 px-2 ml-2 text-sm font-medium leading-loose text-gray-600">Very cool! I'll have to learn more about Tailwind.
+                    </div>
+                    <button className="inline-flex items-center px-1 pt-2 ml-1 flex-column">
+                        <svg className="w-5 h-5 ml-2 text-gray-600 cursor-pointer fill-current hover:text-gray-900" viewBox="0 0 95 78" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M29.58 0c1.53.064 2.88 1.47 2.879 3v11.31c19.841.769 34.384 8.902 41.247 20.464 7.212 12.15 5.505 27.83-6.384 40.273-.987 1.088-2.82 1.274-4.005.405-1.186-.868-1.559-2.67-.814-3.936 4.986-9.075 2.985-18.092-3.13-24.214-5.775-5.78-15.377-8.782-26.914-5.53V53.99c-.01 1.167-.769 2.294-1.848 2.744-1.08.45-2.416.195-3.253-.62L.85 30.119c-1.146-1.124-1.131-3.205.032-4.312L27.389.812c.703-.579 1.49-.703 2.19-.812zm-3.13 9.935L7.297 27.994l19.153 18.84v-7.342c-.002-1.244.856-2.442 2.034-2.844 14.307-4.882 27.323-1.394 35.145 6.437 3.985 3.989 6.581 9.143 7.355 14.715 2.14-6.959 1.157-13.902-2.441-19.964-5.89-9.92-19.251-17.684-39.089-17.684-1.573 0-3.004-1.429-3.004-3V9.936z" fill-rule="nonzero" />
+                        </svg>
+                    </button>
+                    <button className="inline-flex items-center px-1 -ml-1 flex-column">
+                        <svg className="w-5 h-5 text-gray-600 cursor-pointer hover:text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
     </div>
+</section>
+
       
     </div>
   );
