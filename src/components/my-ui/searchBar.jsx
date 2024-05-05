5febb3c2c1544bd7a16b9a6171c6a705b2290cb6
rachidfrex <2003043000250@ofppt-edu.ma>
2024-05-05 18:50:02 +0100

    Add search functionality to SearchBar component

--- src/components/my-ui/searchBar.jsx
+++ src/components/my-ui/searchBar.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import { Search } from 'lucide-react'
 import { useContext } from 'react'
 import { UserContext } from '../useContext'
 import { Link } from 'react-router-dom'
 function SearchBar({search}) {
 const [searchInput, setSearchInput] = useState('');
+const [searchResults, setSearchResults] = useState([]);
+const [buttonClicked, setButtonClicked] = useState(false);
 const { getBlogs } = useContext(UserContext);
-const filteredBlogs = getBlogs.filter(blog => blog.title.toLowerCase().includes(searchInput));
-console.log("filteredBlogs", filteredBlogs);
+
+console.log("filteredBlogs", searchResults);
+console.log("searchInput", searchInput);
+console.log("buttonClicked", buttonClicked);
+function filterBlogs() {
+  const filteredBlogs = getBlogs.filter(blog => blog.title.toLowerCase().includes(searchInput.toLowerCase()));
+   setSearchResults(filteredBlogs);
+  return filteredBlogs;
+}
+
+useEffect(() => {
+  filterBlogs();
+}
+, [searchInput]);
+const handleClick = () => {
+  setSearchInput('');
+}
+     
+
+
 
   return (
     <div
@@ -20,6 +40,7 @@ console.log("filteredBlogs", filteredBlogs);
           <input
             type="text"
             placeholder="Search"
+            value={searchInput}
             onChange={(e) => setSearchInput(e.target.value)}
             className="w-full md:w-auto text-sm bg-slate-100 bg-grey p-3  pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-slate-800 focus:outline-none  focus:ring-2 focus:placeholder:text-black focus:ring-black focus:ring-opacity-50 md:pl-12 "
           />
@@ -29,15 +50,18 @@ console.log("filteredBlogs", filteredBlogs);
 
 
             <div className=' absolute  top-full bg-slate-400 p-4'
-            style={{ display: searchInput.length > 0 ? 'block' : 'none' }}
+            // or botton clicked is true and the input is empty, display the filteredBlogs array
+            style={{ display: (searchInput === '' ) ? 'none' : 'block' }}
             >
             {  // if the input is empty and the filteredBlogs array is empty, display a message
-              filteredBlogs.length === 0
+              searchResults.length === 0
             ? <p className='text-white'>No results found</p>
-            : filteredBlogs.map(blog => (
+            : searchResults.map(blog => (
               <div key={blog.id} className='flex justify-between items-center bg-slate-100 p-2 rounded-md mb-2'>
                 <p>{blog.title}</p>
-                <Link to='./Blog' className='bg-slate-400 text-white p-1 rounded-md'>View</Link>
+                <Link
+                  onClick={handleClick}
+                  to={`/Blog/${blog.id}`}  className='bg-slate-400 text-white p-1 rounded-md'>View</Link>
               </div>
             ))
             }
